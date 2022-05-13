df0e8835616f4111fc464987bc739a73e3bebeeb
akshzt <akyvzz@gmail.com>
2022-05-14 00:10:41 +0530

    redux with react

---
+++ src/features/icecream/IcecreamView.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import { ordered, restocked } from './icecreamSlice';
+
+export const IcecreamView = () => {
+
+    const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
+    const dispatch = useDispatch();
+
+  return (
+    <div>
+        <h2>Number of Icecreams - {numOfIcecreams}</h2>
+        <button onClick={()=>dispatch(ordered())}>Order Icecream</button>
+        <button onClick={()=>dispatch(restocked(2))}>Restock Icecreams</button>
+    </div>
+  )
+}
---
+++ src/features/user/UserView.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { fetchUsers } from './userSlice';
+
+export const UserView = () => {
+
+    const users = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    useEffect(() => {  
+        dispatch(fetchUsers());
+    }, []);
+
+  return (
+    <div>
+        <h2>List of Users</h2>
+        {
+            users.loading && <div>Loading...</div>
+        }
+        {
+            !users.loading && users.error ? <div>Error: {users.error}</div> : null
+        }
+        {
+            !users.loading && users.users.length ? 
+            (
+                <ul>
+                    {
+                        users.users.map(user => (
+                            <li key={user.id}>{user.name}</li>
+                        ))
+                    }
+                </ul>
+            ) :
+            null
+        }
+    </div>
+  )
+}
